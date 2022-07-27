f7b03c4045f6c2d23541ad324d0d21ac23d5e40a
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2022-07-27 13:59:07 +0300

    FilPulse APIs

---
+++ api.js
@@ -0,0 +1,67 @@
+const config = require('./config');
+const { INFO, ERROR, WARNING } = require('./logs');
+const { Pool } = require("pg");
+
+const pool = new Pool(config.database);
+
+function error_response(code, msg, res) {
+    res.status(code).send(msg);
+}
+
+// GET
+const get = async function (query, view, req, res, next, log, single_line = false) {
+    try {
+        var result = await pool.query(`SELECT ${query} FROM ${view};`);
+
+        if (result.rows.length) {
+            INFO(`GET[${log}]: ${JSON.stringify(result.rows)}`);
+
+            if (single_line) {
+                res.json(result.rows[0]);
+            } else {
+                res.json(result.rows);
+            }
+        } else {
+            ERROR(`GET[${log} ${query} on ${view}]: Failed, result: ${JSON.stringify(result.rows)}`);
+            error_response(402, `Failed to get ${log}`, res);
+        }
+
+    } catch (e) {
+        ERROR(`GET[${log} ${query} on ${view}]: error: ${e}`);
+        error_response(401, `Failed to get ${log}`, res);
+    }
+};
+
+const overview = async function (req, res, next) {
+    await  get('*', 'overview_view', req, res, next, 'overview', true);
+};
+
+const top_contributors = async function (req, res, next) {
+    await  get('*', 'top_contributors_view', req, res, next, 'top_contributors');
+};
+
+const commits = async function (req, res, next) {
+    await  get('*', 'commits_view', req, res, next, 'commits');
+};
+
+const issues = async function (req, res, next) {
+    await  get('*', 'issues_view', req, res, next, 'issues', true);
+};
+
+const active_contributors = async function (req, res, next) {
+    await  get('*', 'active_contributors_view', req, res, next, 'active_contributors');
+};
+
+const recent_commits = async function (req, res, next) {
+    await  get('*', 'recent_commits_view', req, res, next, 'recent_commits');
+};
+
+
+module.exports = {
+    overview,
+    top_contributors,
+    commits,
+    issues,
+    active_contributors,
+    recent_commits
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+const config = require('./config');
+const { INFO } = require('./logs');
+const { 
+    overview,
+    top_contributors,
+    commits,
+    issues,
+    active_contributors,
+    recent_commits
+} = require('./api');
+
+var express = require("express");
+var cors = require('cors');
+var app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/overview", overview);
+app.get("/top_contributors", top_contributors);
+app.get("/commits", commits);
+app.get("/issues", issues);
+app.get("/active_contributors", active_contributors);
+app.get("/recent_commits", recent_commits);
+
+
+app.listen(config.api.port, () => {
+    INFO("Startup");
+    INFO("FilPulse API running on port: " + config.api.port);
+   });
